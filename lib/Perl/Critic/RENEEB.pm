aa883d974941bf6f66361e721a1b60a48720fc5f
reneeb <info@perl-services.de>
2018-11-28 01:07:47 +0100

    describe new policy in main module

--- lib/Perl/Critic/RENEEB.pm
+++ lib/Perl/Critic/RENEEB.pm
@@ -21,6 +21,10 @@ The rules included with the Perl::Critic::RENEEB group include:
 I use split with regular expressions regularly, but I don't want to use the x-modifier there. So
 I wrote this policy to check all regular expressions in my programs but those used as a parameter to split.
 
+=head2 L<Perl::Critic::Policy::Reneeb::ProhibitBlockEval>
+
+Use C<try{...}> from L<Try::Tiny|https://metacpan.org/pod/Try::Tiny> instead of C<eval{...}>.
+
 =cut
 
 1; # End of Perl::Critic::RENEEB
