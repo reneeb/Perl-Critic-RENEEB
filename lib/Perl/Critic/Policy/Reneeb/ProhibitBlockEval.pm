af7fc97dff39a22bf8003ceea5237de18361f0bf
reneeb <info@perl-services.de>
2018-11-28 01:07:34 +0100

    add new policy to avoid block eval

---
+++ lib/Perl/Critic/Policy/Reneeb/ProhibitBlockEval.pm
@@ -0,0 +1,54 @@
+package Perl::Critic::Policy::Reneeb::ProhibitBlockEval;
+
+# ABSTRACT: Do not use the Block-eval. Use Try::Tiny instead
+
+use 5.006001;
+use strict;
+use warnings;
+use Readonly;
+
+use Perl::Critic::Utils qw{ :severities };
+
+use base 'Perl::Critic::Policy';
+
+our $VERSION = '0.03';
+
+#-----------------------------------------------------------------------------
+
+Readonly::Scalar my $DESC => q{Prohibit Block-eval};
+Readonly::Scalar my $EXPL => [ 237 ];
+
+#-----------------------------------------------------------------------------
+
+sub default_severity     { return $SEVERITY_MEDIUM           }
+sub default_themes       { return qw<reneeb> }
+sub applies_to           {
+    return qw<
+        PPI::Statement
+    >;
+}
+
+#-----------------------------------------------------------------------------
+
+sub violates {
+    my ( $self, $elem, $doc ) = @_;
+
+    return if $elem->schild ne 'eval';
+    return $self->violation( $DESC, $EXPL, $elem );
+}
+
+1;
+
+__END__
+
+#-----------------------------------------------------------------------------
+
+=pod
+
+=head1 DESCRIPTION
+
+L<Try::Tiny|https://metacpan.org/pod/Try::Tiny> adds some syntactic sugar to your Perl programs.
+It avoids some quirks with exception handling that uses C<eval{...}>.
+
+=cut
+
